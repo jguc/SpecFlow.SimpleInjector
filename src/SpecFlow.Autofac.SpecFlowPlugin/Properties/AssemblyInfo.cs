cc8099ca084fe0fb15071ebe9b732c78ef44df42
Jakub Guc <kubagc@gmail.com>
2017-04-25 00:05:05 +0200

    Naming refactor

--- src/SpecFlow.Autofac.SpecFlowPlugin/Properties/AssemblyInfo.cs
+++ src/SpecFlow.Autofac.SpecFlowPlugin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpecFlow.Autofac (SpecFlow plugin)")]
+[assembly: AssemblyTitle("SpecFlow.SimpleInjector (SpecFlow plugin)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Spec Solutions")]
-[assembly: AssemblyProduct("SpecFlow.Autofac")]
-[assembly: AssemblyCopyright("Copyright © 2016 Gaspar Nagy, Spec Solutions")]
+[assembly: AssemblyProduct("SpecFlow.SimpleInjector")]
+[assembly: AssemblyCopyright("Copyright © 2017 Jakub Guc")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-localdev")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
